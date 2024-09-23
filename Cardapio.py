aa214e83e13a7d29017fef74a7f33955b1bd882f
LeonDev633 <leonardo.dev667622969@gmail.com>
2024-09-23 19:06:54 +0000

    Cardapio inicio

---
+++ Cardapio.py
@@ -0,0 +1,17 @@
+"""Exibir um menu com 7 pratos, apresentando o código do prato e o nome.
+O usuário poderá inserir o código do prato desejado. Caso o código seja inválido, o sistema deve alertar o usuário e pedir novamente um código válido.
+O sistema deverá perguntar ao usuário se ele deseja fazer outro pedido e, se sim, permitir a adição de mais pratos ao pedido.
+Acumular os valores de cada prato escolhido.
+Se o usuário digitar o código "0", o programa encerrará o pedido e calculará o valor total.
+O sistema deve solicitar a forma de pagamento:
+À vista (desconto de 10% sobre o valor total).
+Cartão de crédito (acréscimo de 10% sobre o valor total).
+Exibir os resultados ao final:
+A lista com os códigos e nomes dos pratos escolhidos.
+O subtotal (valor total sem acréscimo ou desconto).
+A forma de pagamento escolhida.
+O valor do desconto ou acréscimo aplicado.
+O valor final a ser pago."""
+
+import os
+os.system("cls||clear")
